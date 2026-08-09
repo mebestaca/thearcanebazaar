1050795ec0e0be518f0dc4d70714b98331803f7a
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-09 17:22:10 -0600

    added category interface

--- types/index.ts
+++ types/index.ts
@@ -6,9 +6,15 @@ export interface Product {
   image_url: string;
   stock: number;
   created_at?: string;
+  category_id: string
 }
 
 export interface CartItem {
   product: Product;
   quantity: number;
 }
+
+export interface Category {
+  id: string;
+  name: string;
+};
