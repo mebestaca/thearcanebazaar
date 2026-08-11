06ec02db7fa8ab942e57a3e34d95d8ad90efd74c
Marc Edison Estaca <mebestaca@gmail.com>
2026-08-11 05:41:13 -0600

    plugged in auth listener

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import AuthListener from "@/components/AuthListener";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <AuthListener />
         <NavBar/>
         {children}
         <Footer/>
